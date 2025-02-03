c0d5bbb974addf04d3325267944135b561ccd243
alvaro <alvaro96m@hotmail.com>
2025-02-03 16:55:59 +0100

    Add documentation

--- include/Utils.h
+++ include/Utils.h
@@ -6,7 +6,27 @@
 
 std::string generateSimpleUUID();
 
+/**
+ * @brief Prepares a SQL query for selecting a specific Element with Int ID
+ *
+ * @param table to seach in
+ * @param colName the name of the column
+ * @param id the param to search for
+ * @return std::string
+ */
 std::string selectWithId(const std::string &table, const std::string &colName, const int id);
+
+/**
+ * @brief Prepares a SQL query for selecting all elements from a table.
+ *
+ * @param table
+ * @return std::string
+ */
 std::string selectAllData(const std::string &table);
 
+/**
+ * @brief Get the Current Time in SQL format
+ *
+ * @return std::string
+ */
 std::string getCurrentDateTime();
