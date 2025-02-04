24f166d2f4692fef8a89b9cf1c85523541393904
alvaro <alvaro96m@hotmail.com>
2025-02-04 14:18:34 +0100

    document TotalDepositORM

--- include/ORM/TotalDepositORM.h
+++ include/ORM/TotalDepositORM.h
@@ -6,29 +6,40 @@
 
 #include "Utils.h"
 #include "CardORM.h"
+#include "HistoryORM.h"
 
+const std::string TOTAL_DEPOSIT__AMOUNT = "amount";
+
+/**
+ * @brief This class acts as an ORM for getting al the money
+ *        in the history given a credit card. It can only read data.
+ *
+ */
 class TotalDepositORM : public IBaseORM
 {
-    CardORM userCard;
+    int account_id;
     int _amount;
-    void setAmount(int amount) { _amount = amount; }
+    void setAmount(int amount) { _amount = amount; } // internal setter
 
 public:
-    explicit TotalDepositORM(CardORM userCard) : userCard(userCard)
+    // Constructor: needs a CardORM object. Other constructors may be useful in different cases.
+    explicit TotalDepositORM(CardORM userCard) : account_id(userCard.getAccountId())
     {
-        setters["amount"] = [this](const std::string &value)
+        setters[TOTAL_DEPOSIT__AMOUNT] = [this](const std::string &value)
         { this->setAmount(std::stoi(value)); };
     }
 
+    // Get the amount
     int getAmount() { return _amount; }
 
+    // Write the query
     std::string load() override
     {
         std::stringstream sql;
-        sql << "SELECT SUM(t.amount) AS amount "
-            << "FROM AccountHistory ah "
-            << "JOIN TransactionTable t ON ah.transaction_id = t.transaction_id "
-            << "WHERE ah.account_id = " << userCard.getAccountId() << ";";
+        sql << "SELECT SUM(t." << TOTAL_DEPOSIT__AMOUNT << ") AS amount "
+            << "FROM " << HISTORY_TABLE_NAME << " ah "
+            << "JOIN " << TRANSTACTION_TABLE_NAME << " t ON ah.transaction_id = t.transaction_id "
+            << "WHERE ah.account_id = " << account_id << ";";
         return sql.str();
     }
 
